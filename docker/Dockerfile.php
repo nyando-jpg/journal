4fdcc886f2c2be7f62a034536de0e2e06dc6289b
nyando-jpg <nyandoratovonjoely@gmail.com>
2026-03-27 23:09:09 +0300

    configuration de base

--- docker/Dockerfile.php
+++ docker/Dockerfile.php
@@ -5,8 +5,22 @@ RUN a2enmod rewrite && \
     a2enmod headers && \
     a2enmod deflate
 
-# Installer les extensions PHP nécessaires (si MySQL n'est pas disponible par défaut)
+# Installer les extensions PHP nécessaires
 RUN docker-php-ext-install pdo pdo_mysql
+
+# Installer Composer
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+# Copier le projet
+WORKDIR /var/www
+COPY . .
+
+# Installer les dépendances PHP avec Composer
+RUN composer install --no-dev --optimize-autoloader
+
+# Crédits des permissions pour Apache
+RUN chown -R www-data:www-data /var/www && \
+    chmod -R 755 /var/www
 
 # Exposer le port
 EXPOSE 80
