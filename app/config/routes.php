99f01726a966047c16666a64f2f59cac3d86060d
laingotiana <ratovoarinirinalaingotiana@gmail.com>
2026-03-28 21:57:40 +0300

    creation des routes

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,22 +1,38 @@
 <?php
 use app\controllers\UserController;
+use app\controllers\ArticleController;
 use flight\Engine;
 use flight\net\Router;
 
 require_once 'services.php';
 
-/** 
- * @var Router $router 
+/**
+ * @var Router $router
  * @var Engine $app
  */
 $userModel = Flight::UserModel();
 $UserController = new UserController($userModel);
+
+$articleModel = Flight::ArticleModel();
+$ArticleController = new ArticleController($articleModel);
 
 // Routes principales
 $router->get('/', [$UserController, 'logPageAdmin']);
 $router->get('/loginAdmin', [$UserController, 'logPageAdmin']);
 $router->post('/login_admin', [$UserController, 'loginUser']);
 $router->get('/pageAdmin', [$UserController, 'pageAdmin']);
+
+// Routes CRUD Articles (BackOffice)
+$router->get('/admin/articles', [$ArticleController, 'index']);
+$router->get('/admin/articles/create', [$ArticleController, 'create']);
+$router->post('/admin/articles/store', [$ArticleController, 'store']);
+$router->get('/admin/articles/edit/@id', [$ArticleController, 'edit']);
+$router->post('/admin/articles/update/@id', [$ArticleController, 'update']);
+$router->get('/admin/articles/delete/@id', [$ArticleController, 'delete']);
+
+// Routes FrontOffice (public)
+$router->get('/actualites', [$ArticleController, 'home']);           // Liste des articles
+$router->get('/article/@id', [$ArticleController, 'show']);          // Détail d'un article
 
 
 
