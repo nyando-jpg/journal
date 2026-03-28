ab1b21cc2be62761ffd7b675279d89d14015e8e1
laingotiana <ratovoarinirinalaingotiana@gmail.com>
2026-03-28 21:54:33 +0300

    configuration du dockerfile

--- docker/Dockerfile.php
+++ docker/Dockerfile.php
@@ -5,8 +5,15 @@ RUN a2enmod rewrite && \
     a2enmod headers && \
     a2enmod deflate
 
+# Installer les dépendances système (unzip et git pour Composer)
+RUN apt-get update && apt-get install -y \
+    unzip \
+    git \
+    libzip-dev \
+    && rm -rf /var/lib/apt/lists/*
+
 # Installer les extensions PHP nécessaires
-RUN docker-php-ext-install pdo pdo_mysql
+RUN docker-php-ext-install pdo pdo_mysql zip
 
 # Installer Composer
 RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
